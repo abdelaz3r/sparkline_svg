b8d9f77bf4196b55aa8ffe5368191298e4f12e82
abdelaz3r <gil.clavien@gmail.com>
2024-01-10 17:13:31 +0100

    Create mix project

---
+++ lib/simple_charts.ex
@@ -0,0 +1,18 @@
+defmodule SimpleCharts do
+  @moduledoc """
+  Documentation for `SimpleCharts`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> SimpleCharts.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/simple_charts_test.exs
@@ -0,0 +1,8 @@
+defmodule SimpleChartsTest do
+  use ExUnit.Case
+  doctest SimpleCharts
+
+  test "greets the world" do
+    assert SimpleCharts.hello() == :world
+  end
+end
