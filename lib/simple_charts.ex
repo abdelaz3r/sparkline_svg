ce5e56a12cb484f9792fe83504ba6883daaca44f
abdelaz3r <gil.clavien@gmail.com>
2024-01-12 09:01:12 +0100

    Rename as_background_image/1 to as_data_uri/1

--- lib/simple_charts.ex
+++ lib/simple_charts.ex
@@ -7,16 +7,16 @@ defmodule SimpleCharts do
   @type svg :: String.t()
 
   @doc """
-  Convert a svg string into a Base64 string to be used as a background-image.
+  Convert a svg string into a Base64 string to be used, for example. as a background-image.
 
   ## Examples
 
-      iex> SimpleCharts.as_background_image(svg_string)
+      iex> SimpleCharts.as_data_uri(svg_string)
       "data:image/svg+xml,%3Csvg..."
 
   """
-  @spec as_background_image(svg()) :: String.t()
-  def as_background_image(svg) when is_binary(svg) do
+  @spec as_data_uri(svg()) :: String.t()
+  def as_data_uri(svg) when is_binary(svg) do
     ["data:image/svg+xml;base64", Base.encode64(svg)] |> Enum.join(",")
   end
 end
