d23eea70125267a600f19626291bcd156f042e84
abdelaz3r <gil.clavien@gmail.com>
2024-02-05 11:26:11 +0100

    Update minimal supported version

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule Sparkline.MixProject do
     [
       app: :sparkline,
       version: "0.1.0",
-      elixir: "~> 1.14",
+      elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
       deps: deps()
     ]
