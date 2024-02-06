260b570724cc7f6de450eeeb677be72271027b57
abdelaz3r <gil.clavien@gmail.com>
2024-02-06 14:43:43 +0100

    Add tests; fix bugs

--- test/sparkline_test.exs
+++ test/sparkline_test.exs
@@ -35,29 +35,28 @@ defmodule SparklineTest do
   end
 
   test "to_svg/2 with empty chart" do
-    empty_chart = """
-    <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n</svg>
-    """
-
     assert Sparkline.new([])
            |> Sparkline.show_dots()
            |> Sparkline.show_line()
            |> Sparkline.show_area()
-           |> Sparkline.to_svg() == {:ok, empty_chart}
+           |> Sparkline.to_svg() ==
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n</svg>
+              """}
   end
 
   test "to_svg/2 with empty chart and placeholder" do
-    one_point_chart = """
-    <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  <text x=\"50%\" y=\"50%\" text-anchor=\"middle\">\n  No data\n</text>\n\n</svg>
-    """
-
     assert Sparkline.new([], placeholder: "No data") |> Sparkline.to_svg() ==
-             {:ok, one_point_chart}
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  <text x="50%" y="50%" text-anchor="middle">\n  No data\n</text>\n\n</svg>
+              """}
   end
 
   test "to_svg/2 with one point chart" do
     one_point_chart_dots = """
-    <svg width=\"100%\" height=\"100%\"\n  viewBox=\"0 0 200 100\"\n  xmlns=\"http://www.w3.org/2000/svg\">\n  \n  \n  <circle\n  cx=\"100.0\"\n  cy=\"50.0\"\n  r=\"1\"\n  fill=\"black\" />\n\n</svg>
+    <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  \n  <circle\n  cx="100.0"\n  cy="50.0"\n  r="1"\n  fill="black" />\n\n</svg>
     """
 
     assert Sparkline.new([{1, 0}]) |> Sparkline.show_dots() |> Sparkline.to_svg() ==
@@ -66,26 +65,24 @@ defmodule SparklineTest do
     assert Sparkline.new([{1, 0}, {1, 2}]) |> Sparkline.show_dots() |> Sparkline.to_svg() ==
              {:ok, one_point_chart_dots}
 
-    one_point_chart_line = """
-    <svg width=\"100%\" height=\"100%\"\n  viewBox=\"0 0 200 100\"\n  xmlns=\"http://www.w3.org/2000/svg\">\n  \n  <path\n  d=\"M80.0,50.0L120.0,50.0\"\n  fill=\"none\"\n  stroke=\"black\"\n  stroke-width=\"0.25\" />\n\n  \n</svg>
-    """
-
     assert Sparkline.new([{1, 0}]) |> Sparkline.show_line() |> Sparkline.to_svg() ==
-             {:ok, one_point_chart_line}
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M80.0,50.0L120.0,50.0"\n  fill="none"\n  stroke="black"\n  stroke-width="0.25" />\n\n  \n</svg>
+              """}
   end
 
   test "to_svg/2 with only zeros as values" do
-    only_zero_chart = """
-    <svg width=\"100%\" height=\"100%\"\n  viewBox=\"0 0 200 100\"\n  xmlns=\"http://www.w3.org/2000/svg\">\n  \n  <path\n  d=\"M6.0,50.0C43.6,50.0 156.4,50.0 194.0,50.0\"\n  fill=\"none\"\n  stroke=\"black\"\n  stroke-width=\"0.25\" />\n\n  \n</svg>
-    """
-
     assert Sparkline.new([{1, 0}, {2, 0}]) |> Sparkline.show_line() |> Sparkline.to_svg() ==
-             {:ok, only_zero_chart}
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M6.0,50.0C43.6,50.0 156.4,50.0 194.0,50.0"\n  fill="none"\n  stroke="black"\n  stroke-width="0.25" />\n\n  \n</svg>
+              """}
   end
 
   test "to_svg/2 with various type of datapoints" do
     chart = """
-    <svg width=\"100%\" height=\"100%\"\n  viewBox=\"0 0 200 100\"\n  xmlns=\"http://www.w3.org/2000/svg\">\n  \n  <path\n  d=\"M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0\"\n  fill=\"none\"\n  stroke=\"black\"\n  stroke-width=\"0.25\" />\n\n  \n</svg>
+    <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0"\n  fill="none"\n  stroke="black"\n  stroke-width="0.25" />\n\n  \n</svg>
     """
 
     assert Sparkline.new([{1, 1}, {2, 2}])
@@ -132,6 +129,57 @@ defmodule SparklineTest do
            |> Sparkline.to_svg() == {:ok, chart}
   end
 
+  test "to_svg/2 with non-default options" do
+    assert Sparkline.new([{1, 1}, {2, 2}], width: 10, height: 10, padding: 1)
+           |> Sparkline.to_svg() ==
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 10 10"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  \n  \n</svg>
+              """}
+  end
+
+  test "to_svg/2 with non-default options (for dots)" do
+    assert Sparkline.new([{1, 1}, {2, 2}])
+           |> Sparkline.show_dots(radius: 2, color: "red")
+           |> Sparkline.to_svg() ==
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  \n  <circle\n  cx="6.0"\n  cy="94.0"\n  r="2"\n  fill="red" />\n<circle\n  cx="194.0"\n  cy="6.0"\n  r="2"\n  fill="red" />\n\n</svg>
+              """}
+  end
+
+  test "to_svg/2 with non-default options (for line)" do
+    assert Sparkline.new([{1, 1}, {2, 2}])
+           |> Sparkline.show_line(width: 1, color: "red", smoothing: 0)
+           |> Sparkline.to_svg() ==
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M6.0,94.0C6.0,94.0 194.0,6.0 194.0,6.0"\n  fill="none"\n  stroke="red"\n  stroke-width="1" />\n\n  \n</svg>
+              """}
+  end
+
+  test "to_svg/2 with non-default options (for area)" do
+    assert Sparkline.new([{1, 1}, {2, 2}])
+           |> Sparkline.show_area(color: "red")
+           |> Sparkline.to_svg() ==
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  <path\n  d="M6.0,94.0C6.0,94.0 194.0,6.0 194.0,6.0V100H6.0Z"\n  fill="red"\n  stroke="none" />\n\n  \n  \n</svg>
+              """}
+  end
+
+  test "to_svg/2 with non-default options (all)" do
+    assert Sparkline.new([{1, 1}, {2, 2}], width: 10, height: 10, padding: 1)
+           |> Sparkline.show_dots(radius: 2, color: "red")
+           |> Sparkline.show_line(width: 1, color: "red", smoothing: 0)
+           |> Sparkline.show_area(color: "red")
+           |> Sparkline.to_svg() ==
+             {:ok,
+              """
+              <svg width="100%" height="100%"\n  viewBox="0 0 10 10"\n  xmlns="http://www.w3.org/2000/svg">\n  <path\n  d="M1.0,9.0C1.0,9.0 9.0,1.0 9.0,1.0V10H1.0Z"\n  fill="red"\n  stroke="none" />\n\n  <path\n  d="M1.0,9.0C1.0,9.0 9.0,1.0 9.0,1.0"\n  fill="none"\n  stroke="red"\n  stroke-width="1" />\n\n  <circle\n  cx="1.0"\n  cy="9.0"\n  r="2"\n  fill="red" />\n<circle\n  cx="9.0"\n  cy="1.0"\n  r="2"\n  fill="red" />\n\n</svg>
+              """}
+  end
+
   test "as_data_uri/1" do
     assert [{1, 1}, {2, 2}]
            |> Sparkline.new()
@@ -140,16 +188,4 @@ defmodule SparklineTest do
            |> Sparkline.as_data_uri() ==
              "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIgogIHZpZXdCb3g9IjAgMCAyMDAgMTAwIgogIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgCiAgPHBhdGgKICBkPSJNNi4wLDk0LjBDNDMuNiw3Ni40IDE1Ni40LDIzLjYgMTk0LjAsNi4wIgogIGZpbGw9Im5vbmUiCiAgc3Ryb2tlPSJibGFjayIKICBzdHJva2Utd2lkdGg9IjAuMjUiIC8+CgogIAo8L3N2Zz4K"
   end
-
-  # TODO: test svg rendered
-  # test "to_svg/2 with non-default options", context do
-  #   assert Sparkline.new([{1, 1}, {2, 2}],
-  #            width: 300,
-  #            height: 50,
-  #            show_dots: false,
-  #            show_area: true
-  #          )
-  #          |> Sparkline.to_svg() ==
-  #            {:ok, context.chart_config}
-  # end
 end
