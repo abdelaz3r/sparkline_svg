2b321a7c2b440c15cb66fd6f0671938493ce5f7e
abdelaz3r <gil.clavien@gmail.com>
2024-02-07 14:29:14 +0100

    Rewrite tests using sigils for better readability

--- test/sparkline_test.exs
+++ test/sparkline_test.exs
@@ -41,23 +41,18 @@ defmodule SparklineTest do
            |> Sparkline.show_area()
            |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"></svg>'}
   end
 
   test "to_svg/2 with empty chart and placeholder" do
     assert Sparkline.new([], placeholder: "No data") |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  <text x="50%" y="50%" text-anchor="middle">\n  No data\n</text>\n\n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><text x="50%" y="50%" text-anchor="middle">No data</text></svg>'}
   end
 
   test "to_svg/2 with one point chart" do
-    one_point_chart_dots = """
-    <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  \n  <circle\n  cx="100.0"\n  cy="50.0"\n  r="1"\n  fill="black" />\n\n</svg>
-    """
+    one_point_chart_dots =
+      ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><circle cx="100.0" cy="50.0" r="1" fill="black" /></svg>'
 
     assert Sparkline.new([{1, 0}]) |> Sparkline.show_dots() |> Sparkline.to_svg() ==
              {:ok, one_point_chart_dots}
@@ -67,23 +62,18 @@ defmodule SparklineTest do
 
     assert Sparkline.new([{1, 0}]) |> Sparkline.show_line() |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M80.0,50.0L120.0,50.0"\n  fill="none"\n  stroke="black"\n  stroke-width="0.25" />\n\n  \n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><path d="M80.0,50.0L120.0,50.0" fill="none" stroke="black" stroke-width="0.25" /></svg>'}
   end
 
   test "to_svg/2 with only zeros as values" do
     assert Sparkline.new([{1, 0}, {2, 0}]) |> Sparkline.show_line() |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M6.0,50.0C43.6,50.0 156.4,50.0 194.0,50.0"\n  fill="none"\n  stroke="black"\n  stroke-width="0.25" />\n\n  \n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><path d="M6.0,50.0C43.6,50.0 156.4,50.0 194.0,50.0" fill="none" stroke="black" stroke-width="0.25" /></svg>'}
   end
 
   test "to_svg/2 with various type of datapoints" do
-    chart = """
-    <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0"\n  fill="none"\n  stroke="black"\n  stroke-width="0.25" />\n\n  \n</svg>
-    """
+    chart =
+      ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><path d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0" fill="none" stroke="black" stroke-width="0.25" /></svg>'
 
     assert Sparkline.new([{1, 1}, {2, 2}])
            |> Sparkline.show_line()
@@ -140,9 +130,7 @@ defmodule SparklineTest do
     assert Sparkline.new([{1, 1}, {2, 2}], width: 10, height: 10, padding: 1)
            |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 10 10"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  \n  \n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"></svg>'}
   end
 
   test "to_svg/2 with non-default options (for dots)" do
@@ -150,9 +138,7 @@ defmodule SparklineTest do
            |> Sparkline.show_dots(radius: 2, color: "red")
            |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  \n  <circle\n  cx="6.0"\n  cy="94.0"\n  r="2"\n  fill="red" />\n<circle\n  cx="194.0"\n  cy="6.0"\n  r="2"\n  fill="red" />\n\n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><circle cx="6.0" cy="94.0" r="2" fill="red" /><circle cx="194.0" cy="6.0" r="2" fill="red" /></svg>'}
   end
 
   test "to_svg/2 with non-default options (for line)" do
@@ -160,9 +146,7 @@ defmodule SparklineTest do
            |> Sparkline.show_line(width: 1, color: "red")
            |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  \n  <path\n  d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0"\n  fill="none"\n  stroke="red"\n  stroke-width="1" />\n\n  \n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><path d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0" fill="none" stroke="red" stroke-width="1" /></svg>'}
   end
 
   test "to_svg/2 with non-default options (for area)" do
@@ -170,9 +154,7 @@ defmodule SparklineTest do
            |> Sparkline.show_area(color: "red")
            |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 200 100"\n  xmlns="http://www.w3.org/2000/svg">\n  <path\n  d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0V100H6.0Z"\n  fill="red"\n  stroke="none" />\n\n  \n  \n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 200 100" xmlns="http://www.w3.org/2000/svg"><path d="M6.0,94.0C43.6,76.4 156.4,23.6 194.0,6.0V100H6.0Z" fill="red" stroke="none" /></svg>'}
   end
 
   test "to_svg/2 with non-default options (all)" do
@@ -182,9 +164,7 @@ defmodule SparklineTest do
            |> Sparkline.show_area(color: "red")
            |> Sparkline.to_svg() ==
              {:ok,
-              """
-              <svg width="100%" height="100%"\n  viewBox="0 0 10 10"\n  xmlns="http://www.w3.org/2000/svg">\n  <path\n  d="M1.0,9.0C1.0,9.0 9.0,1.0 9.0,1.0V10H1.0Z"\n  fill="red"\n  stroke="none" />\n\n  <path\n  d="M1.0,9.0C1.0,9.0 9.0,1.0 9.0,1.0"\n  fill="none"\n  stroke="red"\n  stroke-width="1" />\n\n  <circle\n  cx="1.0"\n  cy="9.0"\n  r="2"\n  fill="red" />\n<circle\n  cx="9.0"\n  cy="1.0"\n  r="2"\n  fill="red" />\n\n</svg>
-              """}
+              ~S'<svg width="100%" height="100%" viewBox="0 0 10 10" xmlns="http://www.w3.org/2000/svg"><path d="M1.0,9.0C1.0,9.0 9.0,1.0 9.0,1.0V10H1.0Z" fill="red" stroke="none" /><path d="M1.0,9.0C1.0,9.0 9.0,1.0 9.0,1.0" fill="none" stroke="red" stroke-width="1" /><circle cx="1.0" cy="9.0" r="2" fill="red" /><circle cx="9.0" cy="1.0" r="2" fill="red" /></svg>'}
   end
 
   test "as_data_uri/1" do
@@ -193,6 +173,6 @@ defmodule SparklineTest do
            |> Sparkline.show_line()
            |> Sparkline.to_svg!()
            |> Sparkline.as_data_uri() ==
-             "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIgogIHZpZXdCb3g9IjAgMCAyMDAgMTAwIgogIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyI+CiAgCiAgPHBhdGgKICBkPSJNNi4wLDk0LjBDNDMuNiw3Ni40IDE1Ni40LDIzLjYgMTk0LjAsNi4wIgogIGZpbGw9Im5vbmUiCiAgc3Ryb2tlPSJibGFjayIKICBzdHJva2Utd2lkdGg9IjAuMjUiIC8+CgogIAo8L3N2Zz4K"
+             "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTAwJSIgaGVpZ2h0PSIxMDAlIiB2aWV3Qm94PSIwIDAgMjAwIDEwMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48cGF0aCBkPSJNNi4wLDk0LjBDNDMuNiw3Ni40IDE1Ni40LDIzLjYgMTk0LjAsNi4wIiBmaWxsPSJub25lIiBzdHJva2U9ImJsYWNrIiBzdHJva2Utd2lkdGg9IjAuMjUiIC8+PC9zdmc+"
   end
 end
