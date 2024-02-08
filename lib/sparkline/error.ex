56bc29f19c5fb1f3e51811bab8a9b8e9726b58f0
abdelaz3r <gil.clavien@gmail.com>
2024-02-08 16:35:58 +0100

    Add documentation for Sparkline error module

--- lib/sparkline/error.ex
+++ lib/sparkline/error.ex
@@ -1,3 +1,9 @@
 defmodule Sparkline.Error do
+  @moduledoc """
+  An exception raised when datapoints, markers, or options are invalid.
+
+  `Sparkline.Error` exceptions have a single field, :message (a String.t/0), which is public.
+  """
+
   defexception [:message]
 end
