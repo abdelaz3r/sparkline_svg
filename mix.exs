50f10ccea857a2feacf9b9b2e207d331f6d5e1a2
abdelaz3r <gil.clavien@gmail.com>
2024-02-10 00:40:42 +0100

    Setup ex_doc config

--- mix.exs
+++ mix.exs
@@ -1,13 +1,31 @@
 defmodule Sparkline.MixProject do
   use Mix.Project
 
+  @version "0.1.0"
+  @repo_url "https://github.com/abdelaz3r/sparkline"
+
   def project do
     [
       app: :sparkline,
-      version: "0.1.0",
+      name: "Sparkline",
+      description: "Sparkline is an Elixir library to generate SVG sparkline charts.",
+      version: @version,
       elixir: "~> 1.12",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      package: package(),
+      source_url: @repo_url,
+      homepage_url: @repo_url,
+      docs: [
+        main: "Sparkline",
+        extras: [
+          {"README.md", [title: "About"]},
+          {"markdown/EXAMPLES.md", [title: "Examples"]},
+          {"markdown/CHANGELOG.md", [title: "Changelog"]},
+          {"LICENSE", [title: "License"]}
+        ],
+        authors: ["Gil Clavien"]
+      ]
     ]
   end
 
@@ -24,6 +42,22 @@ defmodule Sparkline.MixProject do
       {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
       {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
       {:ex_doc, "~> 0.31", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package() do
+    [
+      files: [
+        "lib",
+        "mix.exs",
+        "README.md",
+        "LICENSE"
+      ],
+      licenses: ["Apache 2.0"],
+      links: %{
+        "GitHub (Elixir library)" => @repo_url
+      },
+      maintainers: ["Gil Clavien"]
     ]
   end
 end
